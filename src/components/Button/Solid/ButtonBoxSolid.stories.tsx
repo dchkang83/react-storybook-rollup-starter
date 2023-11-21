91f7b70efcd253a468e802267bfce236720b91a9
deokjoon.kang <dchkang83@gmail.com>
2023-11-21 16:25:43 +0900

    테스트

--- src/components/Button/Solid/ButtonBoxSolid.stories.tsx
+++ src/components/Button/Solid/ButtonBoxSolid.stories.tsx
@@ -4,7 +4,7 @@ import { ButtonBoxSolid } from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof ButtonBoxSolid> = {
-  title: 'Components/Button/Solid',
+  title: 'Components/Button/SolidButton',
   component: ButtonBoxSolid,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -24,11 +24,14 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    label: 'Button',
+    label: 'Button1231',
     size: 'large',
     type: 'primary',
     state: 'enabled',
-    error: 'false',
+    error: false,
+    // onClick: () => {
+    //   alert('Primary test');
+    // },
   },
 };
 
@@ -38,7 +41,7 @@ export const Secondary: Story = {
     size: 'large',
     type: 'secondary',
     state: 'enabled',
-    error: 'false',
+    error: false,
   },
 };
 
@@ -48,6 +51,6 @@ export const Tertiary: Story = {
     size: 'large',
     type: 'tertiary',
     state: 'enabled',
-    error: 'false',
+    error: false,
   },
 };
--- src/components/Button/Solid/ButtonBoxSolid.tsx
+++ src/components/Button/Solid/ButtonBoxSolid.tsx
@@ -17,15 +17,14 @@ interface ButtonBoxSolidProps {
   // /**
   //  * Button contents
   //  */
-  // label: string;
-  rightIcon: false;
   label: string;
-  leftIcon: false;
+  // rightIcon: boolean;
+  // leftIcon: boolean;
   type?: 'primary' | 'secondary' | 'tertiary';
   size?: 'large' | 'medium' | 'small';
   state: 'enabled' | 'pressed' | 'disabled';
-  error: 'true' | 'false';
-  className: any;
+  error: boolean;
+  // className: string;
   /**
    * Optional click handler
    */
@@ -36,21 +35,22 @@ interface ButtonBoxSolidProps {
  * Primary UI component for user interaction
  */
 export const ButtonBoxSolid = ({
-  rightIcon = false,
+  // rightIcon = false,
+  // leftIcon = false,
+  // className = '',
   label = 'ButtonBoxSolid label',
-  leftIcon = false,
   type,
   size,
   state,
-  error,
-  className,
+  error = false,
+  ...props
 }: ButtonBoxSolidProps) => {
   // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     // <button type="button" className={['storybook-button', `storybook-button--${size}`, mode].join(' ')} style={{ backgroundColor }} {...props}>
     //   {label}
     // </button>
-    <div className={`button-box-solid ${size} ${state} ${type} error-${error}`}>
+    <div className={`button-box-solid ${size} ${state} ${type} error-${error}`} {...props}>
       <div className="button-label">{label}</div>
       {state === 'pressed' && <div className="dim" />}
     </div>
